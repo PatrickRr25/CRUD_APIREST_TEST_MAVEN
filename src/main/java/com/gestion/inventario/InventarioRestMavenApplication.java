82d71a7ecb182e7b09a7b188372eb3f18677c015
Patrick Rosales <patoz@Patricks-MacBook-Air.local>
2023-03-30 20:53:07 -0500

    first_commit

---
+++ src/main/java/com/gestion/inventario/InventarioRestMavenApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioRestMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioRestMavenApplication.class, args);
+	}
+
+}
